de9d79761124a2fa313e2d58088d4fb0d3c404f6
emasdai <43808379+emasdai@users.noreply.github.com>
2023-12-20 23:07:15 +0000

    add makefile

--- NFT-f23/script/Interactions.s.sol
+++ NFT-f23/script/Interactions.s.sol
@@ -3,9 +3,20 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script{
-    function run() external{
-        
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external{    
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid); //
+        mintNftOnContract(mostRecentDeployed);  //menjalankan function mintNftOnContract dengan kontrak terbaru 
     }
+
+    function mintNftOnContract(address contractAddress) public{
+        vm.startBroadcast();
+        BasicNft(contractAddress).minNFT(PUG);
+        vm.stopBroadcast();
+    }    
 }
