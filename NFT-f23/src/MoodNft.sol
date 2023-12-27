e23f7579a3880d55e576f6c0b9208252ce9f19ee
emasdai <43808379+emasdai@users.noreply.github.com>
2023-12-27 02:42:59 +0000

    add deploy test

--- NFT-f23/src/MoodNft.sol
+++ NFT-f23/src/MoodNft.sol
@@ -37,7 +37,7 @@ contract MoodNft is ERC721{
 
     function flipMood(uint256 tokenId) public{
         // hanya owner NFT yang bisa flip mood Nftnya
-        if (!isApprovedForAll(msg.sender, tokenId)){
+        if (getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender){
             revert MoodNft_CantFlipMoodIfNotOwner();
         }
 
