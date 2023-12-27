baae9fe0cf6a4bc5f785d45495ee6af8b9b9b2d1
emasdai <43808379+emasdai@users.noreply.github.com>
2023-12-27 00:19:12 +0000

    add moodNft

--- NFT-f23/src/MoodNft.sol
+++ NFT-f23/src/MoodNft.sol
@@ -3,28 +3,60 @@
 pragma solidity ^0.8.19;
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";    // use for encodeNfts to String
 
 contract MoodNft is ERC721{
 
     uint256 private s_tokenCounter = 0; // untuk menghitung banyaknya token
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImageURI;
+    string private s_happySvgImageURI;
+
+    enum Mood{
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
 
     constructor(                // constructor mengambil dari constructor di NFT721 (name, symbol)
-        string memory sadSvg,
-        string memory happySvg
+        string memory sadSvgImageURI,
+        string memory happySvgImageURI
     ) ERC721("MoonNft","Md"){
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageURI = sadSvgImageURI;
+        s_happySvgImageURI = happySvgImageURI;
     } 
 
     function mintNft() public{  // agar orang bisa mintNft, Minting an NFT means creating a unique token on a blockchain
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;   // defaultnya adalah HAPPY
         s_tokenCounter++;
     }
 
+    function _baseURI() internal pure override returns(string memory){  // digunakan untuk base metadata
+        return "data:application/json;base64,";
+    }
+
     function tokenURI(uint256 tokenId) public view override returns(string memory){
-        
+        string memory imageURI;
+        if(s_tokenIdToMood[tokenId] == Mood.HAPPY){
+            imageURI = s_happySvgImageURI;
+        }else{
+            imageURI = s_sadSvgImageURI;
+        }
+
+        return string(      // untuk encode dari bytes32 ke string
+            abi.encode(
+                _baseURI(),Base64.encode(
+                    bytes(abi.encodePacked(
+                        '{"name: "' , 
+                        name(),
+                        '"image": ',
+                        imageURI,
+                        '"}')
+                    )
+                )
+            )
+        );
     }
 }
