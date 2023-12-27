dde265cf1eb40daf2747798c17170cc0d4860ec5
emasdai <43808379+emasdai@users.noreply.github.com>
2023-12-27 02:25:02 +0000

    add deployTest for Mood Nft

--- NFT-f23/script/DeployMoodNft.s.sol
+++ NFT-f23/script/DeployMoodNft.s.sol
@@ -1,3 +1,19 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";    // use for encodeNfts to String
+
+contract DeployMoodNft is Script{
+    function run() external returns(MoodNft){
+
+    }
+
+    // digunakan untuk base64 -i . . . 
+    function svgToImageURI(string memory svg) public pure returns(string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";   // menjadi prefix
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
---
+++ NFT-f23/test/DeployMoodNftTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract DeployMoodNftTest is Test{
+    
+}
